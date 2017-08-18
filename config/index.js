7347beca8f150bc37d875b0c1a23e051bff123a2
river <837606100@qq.com>
2017-08-18 14:15:20 +0800

    switch port

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 3002,
+    port: 8008,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
