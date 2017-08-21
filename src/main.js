ce8ef581740a630c5a8a11b66aa471535f86172c
river <837606100@qq.com>
2017-08-21 16:39:46 +0800

    remove directives

--- src/main.js
+++ src/main.js
@@ -2,22 +2,12 @@ import Vue from 'vue'
 import router from './router/router'
 import store from './store/'
 import { tabManager } from './libs/tabManager'
-// import ElementUI from 'element-ui'
-// import 'element-ui/lib/theme-default/index.css'
 import 'highlight.js/styles/color-brewer.css'
-import directives from './directives/directives'
-import Marked from './directives/marked'
 import Message from './packages/message/main'
 
 Vue.config.productionTip = false
 
-Vue.use(Marked)
-// Vue.use(ElementUI)
 tabManager(Vue, store, router)
-
-Object.keys(directives).forEach(key => {
-  Vue.directive(key, directives[key])
-})
 
 const install = function (Vue, opts = {}) {
   Vue.prototype.$message = Message
