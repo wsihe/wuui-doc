39bfe7f7beb2fea09269895e773eb07b250946fb
river <837606100@qq.com>
2017-08-28 14:41:05 +0800

    add wuui

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import router from './router/router'
 import store from './store/'
 import { tabManager } from './libs/tabManager'
 import 'highlight.js/styles/color-brewer.css'
+import Wuui from 'wuui'
+import 'wuui/dist/wuui.css'
 
+Vue.use(Wuui)
 Vue.config.productionTip = false
 
 tabManager(Vue, store, router)
